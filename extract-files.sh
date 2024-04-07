383b9b842d4527361d997011632a982f2afe9cf6
jacksproject <contact@jackbd.me>
2024-04-07 18:01:32 +0000

    ishtar: Add extract-utils & update extract-files, setup-makefiles
    Signed-off-by: jacksproject <contact@jackbd.me>

--- extract-files.sh
+++ extract-files.sh
@@ -15,9 +15,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${PWD}"
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
