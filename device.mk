fd91fb61396c4ceacfb50b4d22569e8726b91acd
jacksproject <contact@jackbd.me>
2024-04-11 15:44:59 +0000

    ishtar: Add AAPT configs
    Signed-off-by: jacksproject <contact@jackbd.me>

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
