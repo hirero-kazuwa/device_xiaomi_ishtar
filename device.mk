35ab30167d62fd99a6f320a9738eca5668e78e37
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-04-15 16:52:23 +0000

    ishtar: Introduce Dolby Atmos Converted existing dirac implementation based on observing stock sound effects app and daxservice.
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    Signed-off-by: jacksproject <contact@jackbd.me>
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I2de96ba526284dbbb7eb6c85d1963cf5937e8664

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Dolby
+ PRODUCT_PACKAGES += \
+     XiaomiDolby
+
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
